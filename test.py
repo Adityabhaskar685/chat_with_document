c81fa2efc9931c5cf662d666738cb5f73a1e4d03
Aditya Bhaskar <adityaverma685@gmail.com>
2024-02-19 15:37:43 +0000

    v1

--- test.py
+++
@@ -1,5 +0,0 @@
-from rag.client import RAGClient
-c = RAGClient("/teamspace/studios/this_studio/data/2401.08406.pdf")
-for r in c.stream('what is rag'):
-    print(r , end = '')
-print("-" * 10 , "END" , '-' * 10)
